6fe38e0d269ad1fec9837e534178a3d006e6ee24
Mikael Svenson <miksvenson@gmail.com>
2017-02-03 18:00:18 +0100

    Stub SPFx project #24

---
+++ SPFXPageRedirect/src/webparts/pageRedirect/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPageRedirectStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'pageRedirectStrings' {
+  const strings: IPageRedirectStrings;
+  export = strings;
+}
